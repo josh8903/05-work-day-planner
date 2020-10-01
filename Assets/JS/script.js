8074755cdadf2b7002bbeb8bf7650625cb113394
Josh8903 <thorngren87@gmail.com>
2020-10-01 02:16:18 -0500

    refactored code to make better utilization of for loop

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,39 +1,29 @@
-var currentDay = moment().format("dddd, MMMM Do"),
+var time = ["9:00 AM", "10:00 AM", "11:00 AM", "12:00 PM", "1:00 PM", "2:00 PM", "3:00 PM", "4:00 PM", "5:00 PM"],
     HH = moment().format("HH");
 
-// push current day
-$("#currentDay").html(currentDay);
+// append current day to header
+$("#currentDay").html(moment().format("dddd, MMMM Do"));
 
-// append empty time blocks
-$(".container").append('<div class="time-block"><div class="row"><div class="hour">9:00 am</div><div id="9" class="description"><textarea id="save9t" rows="4" cols ="117"></textarea></div><div class="saveBtn"><i id="save9" class="fa fa-floppy-o"></i></div></div></div>');
-$(".container").append('<div class="time-block"><div class="row"><div class="hour">10:00 am</div><div id="10" class="description"><textarea id="save10t" rows="4" cols ="117"></textarea></div><div class="saveBtn"><i id="save10" class="fa fa-floppy-o"></i></div></div></div>');
-$(".container").append('<div class="time-block"><div class="row"><div class="hour">11:00 am</div><div id="11" class="description"><textarea id="save11t" rows="4" cols ="117"></textarea></div><div class="saveBtn"><i id="save11" class="fa fa-floppy-o"></i></div></div></div>');
-$(".container").append('<div class="time-block"><div class="row"><div class="hour">12:00 pm</div><div id="12" class="description"><textarea id="save12t" rows="4" cols ="117"></textarea></div><div class="saveBtn"><i id="save12" class="fa fa-floppy-o"></i></div></div></div>');
-$(".container").append('<div class="time-block"><div class="row"><div class="hour">1:00 pm</div><div id="13" class="description"><textarea id="save13t" rows="4" cols ="117"></textarea></div><div class="saveBtn"><i id="save13" class="fa fa-floppy-o"></i></div></div></div>');
-$(".container").append('<div class="time-block"><div class="row"><div class="hour">2:00 pm</div><div id="14" class="description"><textarea id="save14t" rows="4" cols ="117"></textarea></div><div class="saveBtn"><i id="save14" class="fa fa-floppy-o"></i></div></div></div>');
-$(".container").append('<div class="time-block"><div class="row"><div class="hour">3:00 pm</div><div id="15" class="description"><textarea id="save15t" rows="4" cols ="117"></textarea></div><div class="saveBtn"><i id="save15" class="fa fa-floppy-o"></i></div></div></div>');
-$(".container").append('<div class="time-block"><div class="row"><div class="hour">4:00 pm</div><div id="16" class="description"><textarea id="save16t" rows="4" cols ="117"></textarea></div><div class="saveBtn"><i id="save16" class="fa fa-floppy-o"></i></div></div></div>');
-$(".container").append('<div class="time-block"><div class="row"><div class="hour">5:00 pm</div><div id="17" class="description"><textarea id="save17t" rows="4" cols ="117"></textarea></div><div class="saveBtn"><i id="save17" class="fa fa-floppy-o"></i></div></div></div>');
-
-// loop over time blocks - check style against current time and pull any saved data
-for (var i = 9; i < 18; i++) {
-    var timeBlock = document.getElementById(i),
-        pullSave = "save" + i + "t";
+// append time blocks to body -- set time styles -- populate saved text
+for (var i = 9; i <= 17; i++) {
+    $(".container").append('<div class="time-block"><div class="row"><div class="hour">' + time[i - 9] + '</div><div id="' + i + '" class="description"><textarea id="save' + i + 't" rows="4" cols ="117"></textarea></div><div class="saveBtn"><i id="save' + i + '" class="fa fa-floppy-o"></i></div></div></div>');
+    var textDiv = document.getElementById(i),
+        text = "save" + i + "t";
     if (i < HH) {
-        timeBlock.classList.add("past");
+        textDiv.classList.add("past");
     }
     if (i === HH) {
-        timeBlock.classList.add("present");
+        textDiv.classList.add("present");
     }
     if (i > HH) {
-        timeBlock.classList.add("future");
+        textDiv.classList.add("future");
     }
-    document.getElementById(pullSave).value = localStorage.getItem(pullSave);
+    document.getElementById(text).value = localStorage.getItem(text);
 }
 
-// onclick for save buttons
+// on click save text to local storage
 $("i").click(function() {
-    var clickSave = $(this).attr("id") + "t",
-        saveThis = document.getElementById(clickSave).value;
-    localStorage.setItem(clickSave, saveThis);
+    var textArea = $(this).attr("id") + "t",
+        text = document.getElementById(textArea).value;
+    localStorage.setItem(textArea, text);
 });
